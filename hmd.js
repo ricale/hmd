176a17a335904abc65fe84ffa86a17b165fc762c
KIM Kangseong <kangseong@adfresca.com>
2014-03-30 13:10:58 +0900

    Fix inline rule bug - 링크 스타일에서 title 속성을 적용하지 않으면 untitled 라고 뜨던 현상 수정

--- hmd.js
+++ hmd.js
@@ -137,7 +137,7 @@ hmd = (function() {
             getRule(/``[\s]*(.+?)[\s]*``/g,                          NEED_REPLACER, function(p1) { return '<code>'+p1.replace(/</g,'&lt;')+'</code>' }),
             getRule(/`([^`]+)`/g,                                    NEED_REPLACER, function(p1) { return '<code>'+p1.replace(/</g,'&lt;')+'</code>' }),
             getRule(/!\[([^\]]+)\][\s]*\(([^\s\)]+)(?: "(.*)")?\)/g, NEED_REPLACER, function(p1,p2,p3) { return '<img src="$2" alt="$1" title="$3">' }),
-            getRule(/\[([^\]]+)\][\s]*\(([^\s\)]+)(?: "(.*)")?\)/g,  NEED_REPLACER, function(p1,p2,p3) { return '<a href="'+p2+'" title="'+p3+'">' }, function(p1) { return p1+'</a>' }),
+            getRule(/\[([^\]]+)\][\s]*\(([^\s\)]+)(?: "(.*)")?\)/g,  NEED_REPLACER, function(p1,p2,p3) { return '<a href="'+p2+'"'+(p3!=undefined ? ' title="'+p3+'"' : '')+'>' }, function(p1) { return p1+'</a>' }),
             getRule(/<(http[s]?:\/\/[^<]+)>/g,                       NEED_REPLACER, function(p1) { return '<a href="'+p1+'">'+p1+'</a>'}),
 
             getRule(/\*\*([^\*\s]{1,2}|\*[^\*\s]|[^\*\s]\*|(?:[^\s].+?[^\s]))\*\*/g, NORMAL, '<strong>$1</strong>'),
@@ -161,8 +161,6 @@ hmd = (function() {
                     for(i in ruleArray) {
                         rules[rules.length] = getRule(ruleArray[i][0], NORMAL, ruleArray[i][1]);
                     }
-
-                    console.log(rules);
                 },
 
                 addReference: function(id, url, title) {
